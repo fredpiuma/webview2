7ad9815702dd58048087e3220e034e7d10db8770
Frederico de Castro <fredpiuma@gmail.com>
2023-03-11 13:20:03 -0300

    fix

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBarManager.HEIGHT;
 export default function App() {
   return (
     <View style={{ width: '100%', height: '100%', backgroundColor: '#000000' }}>
-      <div style={{height:STATUSBAR_HEIGHT,backgroundColor:'#ffffff'}}></div>
+      <Text style={{height:STATUSBAR_HEIGHT,backgroundColor:'#ffffff'}}></Text>
         <WebView
           source={{ uri: 'https://chat.openai.com/chat' }}
           style={{ marginTop: 0 }}
