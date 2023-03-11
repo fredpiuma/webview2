6c33c85b361af837ce44ec962a1f4dbd646c62e1
BuildTools <unconfigured@null.spigotmc.org>
2023-03-11 13:05:14 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, NativeModules } from 'react-native';
+import WebView from 'react-native-webview'
+
+const { StatusBarManager } = NativeModules;
+
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBarManager.HEIGHT;
+
+export default function App() {
+  return (
+    <View style={{ width: '100%', height: '100%', backgroundColor: '#000000', paddingTop: STATUSBAR_HEIGHT }}>
+        <WebView
+          source={{ uri: 'https://chat.openai.com/chat' }}
+          style={{ marginTop: 0 }}
+        />
+      </View>
+  );
+  return (
+    <View style={styles.container}>
+      <Text>teste 2ee!</Text>
+      <StatusBar style="auto" />
+      <WebView
+          source={{ uri: 'https://instagram.lacirbacelar.com' }}
+          style={{ marginTop: 0 }}
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+  }
+});
